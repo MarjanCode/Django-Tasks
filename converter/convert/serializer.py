7d43f30a6cba6b4ca200570b5e91d7b9e2af5dad
MarjanCode <marjan0aliyari@gmail.com>
2025-03-11 00:35:28 +0330

    delete converter

--- converter/convert/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class UnitConversionSerializer(serializers.Serializer):
-    value = serializers.FloatField()
-    from_unit = serializers.CharField()
-    to_unit = serializers.CharField()
